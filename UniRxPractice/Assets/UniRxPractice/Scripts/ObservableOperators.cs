a1ff23501a48417f9f63caa4daeb838812b7c383
segur <segur.opus@gmail.com>
2019-03-18 20:56:26 +0900

    Add ObservableOperators

---
+++ UniRxPractice/Assets/UniRxPractice/Scripts/ObservableOperators.cs
@@ -0,0 +1,11 @@
+﻿using UniRx;
+using System;
+
+// Filterを呼び出す拡張メソッド
+public static class ObservableOperators
+{
+    public static IObservable<T> FilterOperator<T>(this IObservable<T> source, Func<T, bool> conditionalFunc)
+    {
+        return new MyFilter<T>(source, conditionalFunc);
+    }
+}
