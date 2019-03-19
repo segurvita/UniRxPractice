5abad08aef114a2e6b32f33aa5656b068b6c01a8
segur <segur.opus@gmail.com>
2019-03-20 00:23:12 +0900

    Add script of PlayerMover

---
+++ UniRxPractice/Assets/UniRxPractice/Scripts/PlayerPractice/PlayerMover.cs
@@ -0,0 +1,43 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace PlayerPractice
+{
+    //プレイヤの移動処理を行う
+    //タイマが0になったら初期座標に戻す
+    public class PlayerMover : MonoBehaviour
+    {
+        [SerializeField]
+        private TimeCounter _timeCounter;
+
+        private float _moveSpeed = 10.0f;
+
+        void Start()
+        {
+            //タイマを購読
+            _timeCounter.OnTimeChanged
+                .Where(x => x == 0) //タイマが0になった時のみ実行
+                .Subscribe(_ =>
+                {
+                    //タイマが0になったら初期座標に戻る
+                    transform.position = Vector3.zero;
+                });
+        }
+
+        void Update()
+        {
+            //右矢印を押している間移動する
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.position += new Vector3(1, 0, 0) * _moveSpeed * Time.deltaTime;
+            }
+
+            //画面外に出たら削除する
+            if (transform.position.x > 10)
+            {
+                Debug.Log("画面画に出た");
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ UniRxPractice/Assets/UniRxPractice/Scripts/PlayerPractice/TimeCounter.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UniRx;
+using UnityEngine;
+
+namespace PlayerPractice
+{
+    /// <summary>
+    /// カウントダウンしてその時の値を通知する
+    /// 3,2,1,0,(OnCompleted) といったイベントが飛ぶ
+    /// </summary>
+    public class TimeCounter : MonoBehaviour
+    {
+        [SerializeField] private int TimeLeft = 3;
+
+        //タイマストリームの実体はこのSubject
+        private Subject<int> timerSubject = new Subject<int>();
+
+        public IObservable<int> OnTimeChanged
+        {
+            get { return timerSubject; }
+        }
+
+        void Start()
+        {
+            StartCoroutine(TimerCoroutine());
+
+            //現在のカウントを表示
+            timerSubject.Subscribe(x => Debug.Log(x));
+        }
+
+        IEnumerator TimerCoroutine()
+        {
+            yield return null;
+
+            var time = TimeLeft;
+            while (time >= 0)
+            {
+                timerSubject.OnNext(time--);
+                yield return new WaitForSeconds(1);
+            }
+            timerSubject.OnCompleted();
+        }
+    }
+}
