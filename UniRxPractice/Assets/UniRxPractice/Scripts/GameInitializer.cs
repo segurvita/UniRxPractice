0a5e971cb8e5eb885f3de77c7a12d6bd401f82ad
segur <segur.opus@gmail.com>
2019-03-19 23:41:39 +0900

    Add script of practice to send Unit.Default when Initialize flow finish

---
+++ UniRxPractice/Assets/UniRxPractice/Scripts/GameInitializer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UniRx;
+using UnityEngine;
+
+public class GameInitializer : MonoBehaviour
+{
+    //Unit型を利用
+    private Subject<Unit> initializedSubject = new Subject<Unit>();
+
+    /// <summary>
+    /// ゲームの初期化が完了したことを通知する
+    /// </summary>
+    public IObservable<Unit> OnInitializedAsync
+    {
+        get { return initializedSubject; }
+    }
+
+    void Start()
+    {
+        //初期化開始
+        StartCoroutine(GameInitializeCoroutine());
+
+        OnInitializedAsync.Subscribe(_ =>
+        {
+            Debug.Log("初期化完了");
+        });
+    }
+
+    IEnumerator GameInitializeCoroutine()
+    {
+        yield return new WaitForSeconds(1.0f);
+
+        /**
+        初期化処理
+
+        WWWで通信したり、オブジェクトをインスタンス化したりといった
+        時間がかかる重い処理をここでやる想定
+        **/
+
+        //初期化終了を通知する
+        initializedSubject.OnNext(Unit.Default); //タイミングが重要な通知なのでUnitで十分
+        initializedSubject.OnCompleted();
+    }
+}
