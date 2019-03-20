79b8bb72b190f601c4fcce4437c9a0c1e45c30c2
segur <segur.opus@gmail.com>
2019-03-20 15:46:17 +0900

    Add script to practice UpdateAsObservable

---
+++ UniRxPractice/Assets/UniRxPractice/Scripts/Triggers/UpdateSample.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UniRx;
+using UniRx.Triggers; //このusingが必要
+
+public class UpdateSample : MonoBehaviour
+{
+    void Start()
+    {
+        // UpdateAsObservableはComponentに対する
+        // 拡張メソッドとして定義されているため、呼び出す際は
+        // "this."が必要
+        this.UpdateAsObservable()
+            .Subscribe(_ => Debug.Log("Update!"));
+    }
+}
